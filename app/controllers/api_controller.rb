dc345dc14fdcb95393a03cc4728cafd60d2e333c
criscastillo <cristobaljcf@gmail.com>
2016-05-15 19:15:09 -0400

    conteo de posts

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,41 @@
+require 'pg'
+require 'rest-client'
+require 'json'
+
+class ApiController < ApplicationController
+
+	skip_before_filter  :verify_authenticity_token
+
+	$url = 'http://api.instagram.com/v1/tags/'
+
+	def cantidadPosts(tag, token)
+		respuesta = JSON.parse(RestClient.get $url + tag + '?access_token=' + token)
+		puts 'api tag instagram: ' + respuesta.to_s
+
+		total = respuesta["data"]["media_count"]
+		puts 'total posts: ' + total.to_s
+
+		return total
+	end
+
+	#tdi-tarea2.herokuapp.com/api/instagram/tag/buscar
+	def buscar
+		tag = params[:tag].to_s
+		puts 'tag: ' + tag
+		token = params[:access_token].to_s
+		puts 'token: ' + token
+
+		final = {:metadata =>  {:total => cantidadPosts(tag, token)},
+			:posts => [{}],
+			:version => "v1.0.0"}
+
+		puts 'respuesta de la api: ' + final.to_s
+
+		render json: final.to_json, status: 200
+
+		#excepciones
+		rescue Exception => e
+			render json: {}, status: 500
+	end
+
+end
