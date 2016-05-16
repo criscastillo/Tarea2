037e31c934b30b9a633e7be68d0fc5bc50b260ac
criscastillo <cristobaljcf@gmail.com>
2016-05-15 21:26:26 -0400

    api completa

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,7 @@ class ApiController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
 
 	$url = 'http://api.instagram.com/v1/tags/'
+	$version = '1.0.0'
 
 	def cantidadPosts(tag, token)
 		respuesta = JSON.parse(RestClient.get $url + tag + '?access_token=' + token)
@@ -18,6 +19,36 @@ class ApiController < ApplicationController
 		return total
 	end
 
+	def obtenerPosts(tag, token)
+		respuesta = RestClient.get $url + tag + '/media/recent?access_token=' + token
+		posts = JSON.parse(respuesta)["data"]
+
+		return posts
+	end
+
+	def informacionPosts(posts)
+		cantidad = posts.length
+		respuesta = Array.new(cantidad)
+
+		i = 0
+		while i < cantidad do
+			info = {:tags => posts[i]["tags"],
+				:username => posts[i]["user"]["username"],
+				:likes => posts[i]["likes"]["count"],
+				:url => posts[i]["images"]["standard_resolution"]["url"],
+				:caption => posts[i]["caption"]["text"]
+				}
+
+			respuesta[i] = info
+
+			i += 1
+		end
+
+		puts respuesta.to_s
+
+		return respuesta
+	end
+
 	#tdi-tarea2.herokuapp.com/api/instagram/tag/buscar
 	def buscar
 		tag = params[:tag].to_s
@@ -26,8 +57,8 @@ class ApiController < ApplicationController
 		puts 'token: ' + token
 
 		final = {:metadata =>  {:total => cantidadPosts(tag, token)},
-			:posts => [{}],
-			:version => "v1.0.0"}
+			:posts => informacionPosts(obtenerPosts(tag, token)),
+			:version => $version}
 
 		puts 'respuesta de la api: ' + final.to_s
 
--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ApiControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "recibir bien los parámetros" do
+		get(:buscar, {'tag' => 'instachile', 'access_token' => '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'})
+
+		assert_response :success
+    	assert_not_nil assigns(:tag)
+    	assert_not_nil assigns(:token)
+	end
 end
